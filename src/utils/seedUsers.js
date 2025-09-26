640ef6804a7e301dd11e293b55054e8cb062fb80
Guillermo Giuliana <guillermo@opensupports.com>
2025-09-26 01:03:58 -0300

    seed act

--- src/utils/seedUsers.js
+++ src/utils/seedUsers.js
@@ -9,7 +9,7 @@ const users = [
     avatar: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=150&h=150&fit=crop&crop=face"
   },
   {
-    name: "María García",
+    name: "María  Alvarez",
     email: "maria@test.com", 
     password: "123456",
     avatar: "https://images.unsplash.com/photo-1494790108755-2616b612b786?w=150&h=150&fit=crop&crop=face"
